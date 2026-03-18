1a4eea7be1905d3d76fe23480f5126a410c908c8
Sean Murphy <sean@iamseanmurphy.com>
2026-03-18 14:41:15 +0000

    1.6.2

--- sqlite-object-cache/sqlite-object-cache.php
+++ sqlite-object-cache/sqlite-object-cache.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: SQLite Object Cache
- * Version: 1.6.1
+ * Version: 1.6.2
  * Plugin URI: https://github.com/OllieJones/sqlite-object-cache
  * Description: A persistent object cache backend powered by SQLite3.
  * Author: Oliver Jones
  * Author URI: https://github.com/OllieJones/
  * Requires at least: 5.5
  * Requires PHP: 5.6
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * Text Domain: sqlite-object-cache
  * Domain Path: /languages/
  * License: GPLv2 or later
@@ -34,6 +34,7 @@ if ( is_admin()  || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
   require_once 'includes/lib/class-sqlite-object-cache-statistics.php';
   require_once 'includes/lib/class-sqlite-backup-exclusion.php';
   require_once 'includes/lib/class-file.php';
+  require_once 'includes/lib/class-sqlite-object-cache-opcache.php';
 }
 /**
  * Returns the main instance of SQLite_Object_Cache to prevent the need to use globals.
@@ -42,7 +43,7 @@ if ( is_admin()  || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
  * @since  1.0.0
  */
 function sqlite_object_cache() {
-  $instance = new SQLite_Object_Cache( __FILE__, '1.6.1' );
+  $instance = new SQLite_Object_Cache( __FILE__, '1.6.2' );
 
   if ( is_admin() ) {
     $instance->settings = new SQLite_Object_Cache_Settings( $instance, plugin_basename( __FILE__ ));
